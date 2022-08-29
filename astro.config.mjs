19e7e95f883500bef142bb5a1aa99e727501cbab
Martin Beaumont <37398960+martinbmnt@users.noreply.github.com>
2022-08-29 14:43:17 +0200

    change site url in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://martinbeaumont.dev',
+  site: 'https://www.martinbeaumont.dev',
   trailingSlash: 'never',
   build: {
     format: 'file'
